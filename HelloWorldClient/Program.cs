f85c993218b419c004c271093e4514774c2dbf81
Vladimir Shubarin <you@example.com>
2021-07-06 14:55:10 +0300

    Initial commit

---
+++ HelloWorldClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldServiceClient client = new HelloWorldServiceClient();
+            try
+            {
+                Console.WriteLine(client.GetMessage("Vladimir"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR: {0}",e.Message);
+            }
+        }
+    }
+}
